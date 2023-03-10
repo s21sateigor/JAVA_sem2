555187f5d3c070f52371eeff655d7bb1439855d2
Igors Šaters <s21sateigor@venta.lv>
2023-03-10 14:14:16 +0200

    Edit: Course class edit, added course constructor and setters/getters

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,72 +1,86 @@
 package model;
 
-public class Course
-{
+public class Course {
+    //1.variables
     private long id;
     private String title; //only letters, digits and space
-    private int creditPoints; // min = 1, max = 20
-    private CourseGradeType type; // not null
-    private Professor professor; // not null
+    private int creditPoints; //min = 1, max = 20
+    private CourseGradeType type; //not null
+    private Professor professor; //not null
 
+    public Course(String title, int creditPoints, CourseGradeType type, Professor professor){
+        setId();
+        setTitle(title);
+        setCreditPoints(creditPoints);
+        setType(type);
+        setProfessor(professor);
 
+    }
     private static long courseCounter = 12000;
+
+    //2.getters and setters
     public long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    public void setId() {
         this.id = courseCounter;
         courseCounter++;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String inputTitle) {
-        if(inputTitle != null && inputTitle.matches("[a-z ,.'-]")){
-            //[A-Z]{1}[a-zA-Z0-9s]
+        if(inputTitle != null && inputTitle.matches("[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņA-ZĒŪĪĻĶĢŠĀŽČŅ0-9\\s]+")) {
             title = inputTitle;
-        } else {
+        }
+        else
+        {
             title = "Unknown";
         }
     }
-
     public int getCreditPoints() {
         return creditPoints;
     }
-
     public void setCreditPoints(int inputCreditPoints) {
-        if(inputCreditPoints > 0 && inputCreditPoints < 21){
+        if(inputCreditPoints > 0 && inputCreditPoints < 21) {
             creditPoints = inputCreditPoints;
-        } else {
+        }
+        else
+        {
             creditPoints = 0;
         }
-
     }
-
     public CourseGradeType getType() {
         return type;
     }
-
     public void setType(CourseGradeType inputType) {
-        if(inputType != null){
+        if(inputType!=null) {
             type = inputType;
-        } else {
+        }
+        else
+        {
             type = CourseGradeType.other;
         }
     }
-
     public Professor getProfessor() {
         return professor;
     }
-
     public void setProfessor(Professor inputProfessor) {
-        if(inputProfessor != null){
+        if(inputProfessor != null) {
             professor = inputProfessor;
-        } else {
-            professor = new Professor();
         }
-
+        else
+        {
+            professor = new Professor();//it will call Professor no-args constructor
+        }
     }
+
+
+
+
+
+    //3.constructors
+    //4. toString
+    //5. additional functions
+
 }
